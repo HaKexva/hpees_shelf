995762542cfcc9c6e12e8aa2456d3a014d6ad16f
Ray Chang <ray120424@gmail.com>
2026-02-05 10:21:42 +0800

    HAK-21: Add book tag filter on book list page
    - Book model: TAG_FILTER_OPTIONS for dropdown
    - BooksController#index: filter by params[:tag]
    - Books index: filter by tag dropdown (All tags / 圖書館的書 / 捐贈的書 / 班級的書 / 老師的書), Tag column in table

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,8 @@ class BooksController < ApplicationController
   # GET /books or /books.json
   def index
     @books = Book.all
+    @books = @books.where(tag: params[:tag]) if params[:tag].present?
+    @filter_tag = params[:tag]
   end
 
   # GET /books/1 or /books/1.json
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  TAG_LIBRARY = "圖書館的書"
+  TAG_DONATED = "捐贈的書"
+  TAG_CLASS = "班級的書"
+  TAG_TEACHER_PREFIX = "老師的書"
+  TAG_FILTER_OPTIONS = [ [ "圖書館的書", TAG_LIBRARY ], [ "捐贈的書", TAG_DONATED ], [ "班級的書", TAG_CLASS ], [ "老師的書", TAG_TEACHER_PREFIX ] ].freeze
 end
